2ddcf79ec1a8b38c8e9d61be1650151fc7b74ba2
jitendrasinghchouhan3007 <jsinghchouhan971@gmail.com>
2025-08-20 20:21:05 +0530

    Add files via upload

---
+++ src/pages/Home/Home1.jsx
@@ -0,0 +1,20 @@
+import React, { use, useState } from 'react'
+import './home.css'
+import Header from '../../Components/Header/Header'
+import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay'
+import AppDownload from '../../Components/AppDownload/AppDownload'
+
+const Home1 = () => {
+const[category,setCategory] = useState("All")
+  return (
+    <div>
+      <Header/>
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category}/>
+      <AppDownload/>
+    </div>
+  )
+}
+
+export default Home1
